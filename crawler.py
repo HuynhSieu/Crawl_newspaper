772fec572fb798a6e96fcafdeced2a88808404c4
HuynhSieu <110023949+HuynhSieu@users.noreply.github.com>
2022-07-28 17:49:23 +0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,8 +6,9 @@ import re
 from typing import Callable
 from dataclasses import dataclass
 import logging
+from pathlib import Path
 
-logging.basicConfig(
+logging.basicConfig(filename="log.txt",
     format='%(asctime)s %(levelname)s:%(message)s',
     level=logging.INFO)
 
@@ -20,31 +21,36 @@ class Zing:
         self.subject_links = []
         self.links_of_subject = links
 
-    def get_links(self, url, html):
+    def get_links(self, url):
+        html = requests.get(url).text   
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.findAll('a', recursive=True):
             path = link.get('href')
             if path and path.startswith('/'):
                 path = urljoin(url, path)
-            yield path
-
-    def crawl(self, url):
-        lambda url: url not in self.links_of_subject
-        html = requests.get(url).text
-        for url in self.get_links(url, html):
-            if url not in self.subject_links and url not in self.links_of_subject:
-                self.links_of_subject.append(url)
+                if url not in self.subject_links and url not in self.links_of_subject:
+                    self.links_of_subject.append(path)
+        return self.links_of_subject
 
     def run(self):
         while self.links_of_subject:
             url = self.links_of_subject.pop(0)
             logging.info(f'Crawling: {url}')
             try:
-                self.crawl(url)
+                self.get_links(url)
             except Exception:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
                 self.subject_links.append(url)
+            if url == self.links_of_subject[0]:
+                break
+        subject = pd.DataFrame({'Link_subject': self.subject_links})
+        subject.drop_duplicates(inplace=True)
+        subject.drop(subject.index[0:1], axis = 0, inplace = True)
+        return subject
 
 if __name__ == '__main__':
-    Zing(links=['https://zingnews.vn/']).run()
+    Subject = Zing(links=['https://zingnews.vn/']).run()
+    p = Path('D:/Sieu/crawl_zingnews/news-listening-master/temp')
+    p.mkdir(exist_ok=True)
+    Subject.to_excel(f'{p}/Subject.xlsx', index = False)
