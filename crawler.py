c26b8beaa966a210b666f034ad527a2e3243047b
HuynhSieu <sieu.huynh@innotech.vn>
2022-07-28 14:52:35 +0700

    crawler

--- crawler.py
+++ crawler.py
@@ -8,14 +8,14 @@ logging.basicConfig(
     level=logging.INFO)
 
 class Zing:
-    def __init__(self, urls):
-        self.subject_urls = []
-        self.urls_of_subject = urls
+    def __init__(self, links=[]):
+        self.subject_links = []
+        self.links_of_subject = links
 
     def get_url(self, url):
         return requests.get(url).text
 
-    def get_linked_urls(self, url, html):
+    def get_linked_links(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             path = link.get('href')
@@ -24,24 +24,25 @@ class Zing:
             yield path
 
     def add_url_to_list(self, url):
-        if url not in self.subject_urls and url not in self.urls_of_subject:
-            self.urls_of_subject.append(url)
+        if url not in self.subject_links and url not in self.links_of_subject:
+            self.links_of_subject.append(url)
+        print(self.links_of_subject)
 
     def crawl(self, url):
         html = self.get_url(url)
-        for url in self.get_linked_urls(url, html):
+        for url in self.get_linked_links(url, html):
             self.add_url_to_list(url)
 
     def run(self):
-        while self.urls_of_subject:
-            url = self.urls_of_subject.pop(0)
+        while self.links_of_subject:
+            url = self.links_of_subject.pop(0)
             logging.info(f'Crawling: {url}')
             try:
-                filter(lambda x: x not in self.crawl(url), url)
+                lambda x: x not in self.crawl(url), url
             except Exception:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
-                self.subject_urls.append(url)
+                self.subject_links.append(url)
 
 if __name__ == '__main__':
-    Zing(urls='https://zingnews.vn/').run()
+    Zing(links=['https://zingnews.vn/']).run()
