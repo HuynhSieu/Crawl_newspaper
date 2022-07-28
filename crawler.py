1a8546faf0fd8102210437c32a3d16522d8edf00
HuynhSieu <110023949+HuynhSieu@users.noreply.github.com>
2022-07-28 16:31:04 +0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -12,25 +12,33 @@ logging.basicConfig(
     level=logging.INFO)
 
 class Zing:
-    def __init__(self, links=[]):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    def __init__(self, links):
         self.subject_links = []
         self.links_of_subject = links
 
+
+    def get_parse(self, url):
+        if url not in self.subject_links and url not in self.links_of_subject:
+            return requests.get(url).text
+
     def get_links(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
-        for link in soup.find_all('a'):
+        for link in soup.findAll('a', recursive=True):
             path = link.get('href')
             if path and path.startswith('/'):
                 path = urljoin(url, path)
-            return path
+            yield path
 
     def add_url_to_list(self, url):
         if url not in self.subject_links and url not in self.links_of_subject:
             self.links_of_subject.append(url)
-        print(self.links_of_subject)
 
     def crawl(self, url):
-        html = requests.get(url).text
+        html = self.get_parse(url)
         for url in self.get_links(url, html):
             self.add_url_to_list(url)
 
@@ -39,7 +47,7 @@ class Zing:
             url = self.links_of_subject.pop(0)
             logging.info(f'Crawling: {url}')
             try:
-                lambda x: x not in self.crawl(url), url
+                self.crawl(url)
             except Exception:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
@@ -47,3 +55,4 @@ class Zing:
 
 if __name__ == '__main__':
     Zing(links=['https://zingnews.vn/']).run()
+
